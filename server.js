bc3caff325b136f90d48696269d22e75d5dc7cd9
tuliohs <tuliohhenriquepaz@gmail.com>
2021-04-19 19:34:03 -0300

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const commoncrawl = require('commoncrawl')
+
+let options = {
+    index: 'CC-MAIN-2019-30-index',
+    from: '2018',
+    to: '2019',
+    matchType: 'domain', // exact, prefix, host , domain,
+    limit: 100,
+    page: 1,
+    pageSize: 100,
+    showNumPages: false,
+}
+
+
+//commoncrawl.getIndex().then((data) => {
+//    console.log(data.length);
+//});
+
+commoncrawl.searchURL('example.com/*')
+    .then((data) => {
+        console.log(data);
+    });
